f51d5e9ae9c4ccf3de5fe4b47e93080ece4711c3
AchaUmbaPretre <achandambi@gmail.com>
2024-08-27 15:29:04 +0100

    commit

--- controllers/offres.controller.js
+++ controllers/offres.controller.js
@@ -18,6 +18,62 @@ exports.getOffre = (req, res) => {
     });
 };
 
+exports.getOffreDetailOne = (req, res) => {
+    const {id_offre} = req.query;
+
+    const q = `
+            SELECT 
+                o.nom_offre,
+                p.nom_article,
+                p.prix_unitaire,
+                op.quantite,
+                p.prix_unitaire * op.quantite AS montant
+            FROM 
+                offre_article op
+            JOIN 
+                offres o ON op.id_offre = o.id_offre
+            JOIN 
+                articles p ON op.id_article = p.id_article
+            WHERE 
+                o.id_offre = ${id_offre}
+
+    `;
+
+    db.query(q, (error, data) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        return res.status(200).json(data);
+    });
+};
+
+exports.getOffreArticleOne = (req, res) => {
+    const {id_offre} = req.query;
+
+    const q = `
+        SELECT 
+            o.nom_offre,
+            SUM(p.prix_unitaire * op.quantite) AS total
+        FROM 
+            offre_article op
+        JOIN 
+            offres o ON op.id_offre = o.id_offre
+        JOIN 
+            articles p ON op.id_article = p.id_article
+        WHERE 
+            o.id_offre = ${id_offre}
+        GROUP BY 
+            o.nom_offre;
+    `;
+
+    db.query(q, (error, data) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        return res.status(200).json(data);
+    });
+};
+
 exports.postOffres = async (req, res) => {
 
     try {
@@ -34,6 +90,53 @@ exports.postOffres = async (req, res) => {
     } catch (error) {
         console.error('Erreur lors de l\'ajout de l offre :', error);
         return res.status(500).json({ error: "Une erreur s'est produite lors de l'ajout de la tâche." });
+    }
+};
+
+exports.postOffresArticle = async (req, res) => {
+    const articles = req.body.articles;
+    const id_offre = req.body.id_offre;
+
+    try {
+        const qInsertArticle = 'INSERT INTO articles(`nom_article`, `prix_unitaire`, `id_categorie`) VALUES(?,?,?)';
+        const qInsertOffreArticle = 'INSERT INTO offre_article(`id_offre`, `id_article`, `quantite`) VALUES(?,?,?)';
+
+        for (let article of articles) {
+            const articleValues = [
+                article.nom_article,
+                article.prix_unitaire,
+                article.id_categorie
+            ];
+
+            await new Promise((resolve, reject) => {
+                db.query(qInsertArticle, articleValues, (err, result) => {
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    const insertID = result.insertId;  // Récupération de l'ID de l'article inséré
+                    const offreArticleValues = [
+                        id_offre,
+                        insertID,
+                        article.quantite
+                    ];
+
+                    db.query(qInsertOffreArticle, offreArticleValues, (err) => {
+                        if (err) {
+                            return reject(err);
+                        }
+
+                        resolve();
+                    });
+                });
+            });
+        }
+
+        return res.status(201).json({ message: 'Articles ajoutés avec succès' });
+
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout des articles :', error);
+        return res.status(500).json({ error: "Une erreur s'est produite lors de l'ajout des articles." });
     }
 };
 
--- routes/offres.routes.js
+++ routes/offres.routes.js
@@ -3,6 +3,9 @@ const offreController = require('./../controllers/offres.controller')
 const router = express.Router();
 
 router.get('/', offreController.getOffre)
+router.get('/detail', offreController.getOffreDetailOne)
+router.get('/one_offre', offreController.getOffreArticleOne)
+router.post('/article', offreController.postOffresArticle)
 router.post('/', offreController.postOffres)
 router.delete('/:id', offreController.deleteOffres)
  
