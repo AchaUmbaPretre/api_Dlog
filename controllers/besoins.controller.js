bf1f7511d6b6abbf4b1a08f1c6edc5432e79f823
AchaUmbaPretre <achandambi@gmail.com>
2024-09-13 13:27:39 +0100

    commit

--- controllers/besoins.controller.js
+++ controllers/besoins.controller.js
@@ -18,9 +18,10 @@ exports.getBesoinCount = (req, res) => {
 exports.getBesoin = (req, res) => {
 
     const q = `
-                SELECT besoins.*, articles.nom_article, projet.nom_projet FROM besoins
-                    INNER JOIN articles ON besoins.id_article = articles.id_article
-                    INNER JOIN projet ON besoins.id_projet = projet.id_projet
+                SELECT besoins.description, besoins.quantite, articles.nom_article, projet.nom_projet, client.nom FROM besoins
+                    LEFT JOIN articles ON besoins.id_article = articles.id_article
+                    LEFT JOIN projet ON besoins.id_projet = projet.id_projet
+                    LEFT JOIN client ON projet.client =client.id_client
             `;
 
     db.query(q, (error, data) => {
