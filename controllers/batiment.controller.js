e1f010bc14e0c4f5f9cd1a54525ca3b4091aaa0c
AchaUmbaPretre <achandambi@gmail.com>
2024-09-20 11:04:30 +0100

    commit

--- controllers/batiment.controller.js
+++ controllers/batiment.controller.js
@@ -14,10 +14,34 @@ exports.getEquipement = (req, res) => {
     });
 };
 
-exports.getEquipement = (req, res) => {
+exports.postEquipement = async (req, res) => {
+
+    try {
+        const q = 'INSERT INTO equipments(`id_batiment`, `id_type_batiment`, `model`, `num_serie`, `installation_date`, `maintenance_date`, `location`, `status`) VALUES(?,?,?,?,?,?,?,?)';
+
+        const values = [
+            req.body.id_batiment,
+            req.body.id_type_batiment,
+            req.body.model,
+            req.body.num_serie,
+            req.body.installation_date,
+            req.body.maintenance_date,
+            req.body.location,
+            req.body.status
+        ];
+
+        await db.query(q, values);
+        return res.status(201).json({ message: 'Suivi de tache ajouté avec succès' });
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout de la tâche :', error.message);
+        return res.status(500).json({ error: "Une erreur s'est produite lors de l'ajout de la tâche." });
+    }
+};
+
+exports.getBatimentPlans = (req, res) => {
 
     const q = `
-                SELECT * FROM equipments
+                SELECT * FROM batiment_plans
             `;
 
     db.query(q, (error, data) => {
@@ -26,4 +50,71 @@ exports.getEquipement = (req, res) => {
         }
         return res.status(200).json(data);
     });
+};
+
+exports.postBatimentPlans = async (req, res) => {
+
+    const { id_batiment, nom_document, type_document } = req.body;
+
+    if (!req.files || req.files.length === 0) {
+        return res.status(400).json({ message: 'Aucun fichier téléchargé' });
+    }
+
+    const documents = req.files.map(file => ({
+        chemin_document: file.path.replace(/\\/g, '/'),
+        id_batiment,
+        nom_document,
+        type_document
+    }));
+
+    documents.forEach((doc) => {
+        const query = 'INSERT INTO batiment_plans(`id_batiment`, `nom_document`, `type_document`, `chemin_document`) VALUES(?,?,?,?,?,?,?,?)';
+
+        db.query(query, [doc.id_batiment, doc.nom_document, doc.type_document, doc.chemin_document], (err, result) => {
+            if (err) {
+                console.error('Erreur lors de l\'insertion du document:', err);
+                return res.status(500).json({ message: 'Erreur interne du serveur' });
+            }
+        });
+    });
+
+    res.status(200).json({ message: 'Documents ajoutés avec succès' });
+};
+
+exports.getMaintenance = (req, res) => {
+
+    const q = `
+                SELECT * FROM maintenance_logs
+            `;
+
+    db.query(q, (error, data) => {
+        if (error) {
+            return res.status(500).send(error);
+        }
+        return res.status(200).json(data);
+    });
+};
+
+exports.postMaintenance = async (req, res) => {
+
+    try {
+        const q = 'INSERT INTO maintenance_logs(`id_equipement`, `maintenance_date`, `description`, `num_serie`, `installation_date`, `maintenance_date`, `location`, `status`) VALUES(?,?,?,?,?,?,?,?)';
+
+        const values = [
+            req.body.id_equipement,
+            req.body.maintenance_date,
+            req.body.model,
+            req.body.num_serie,
+            req.body.installation_date,
+            req.body.maintenance_date,
+            req.body.location,
+            req.body.status
+        ];
+
+        await db.query(q, values);
+        return res.status(201).json({ message: 'Suivi de tache ajouté avec succès' });
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout de la tâche :', error.message);
+        return res.status(500).json({ error: "Une erreur s'est produite lors de l'ajout de la tâche." });
+    }
 };
