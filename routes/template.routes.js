020eb97dcd852dfbbf5bc88c2680f71f8c1dba01
AchaUmbaPretre <achandambi@gmail.com>
2024-11-04 13:44:55 +0100

    commit

--- routes/template.routes.js
+++ routes/template.routes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const templateController = require("../controllers/template.controller");
 const router = express.Router();
 
-router.get('/templace', templateController.getTemplate)
-router.get('/template/one', templateController.getTemplateOne)
-router.post('/template', templateController.postTemplate)
+router.get('/', templateController.getTemplate)
+router.get('/one', templateController.getTemplateOne)
+router.post('/', templateController.postTemplate)
 
 module.exports = router;
