cbee52cfa1d1bbba0dc202c9a40aecd778bae2c2
AchaUmbaPretre <achandambi@gmail.com>
2024-12-02 13:16:21 +0100

    commit

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -180,8 +180,8 @@ exports.getProvince = (req, res) => {
 exports.getProvinceOne = (req, res) => {
     const {id} = req.query;
 
-    const q = `SELECT * FROM provinces WHERE id = ?
-    `;
+    const q = `SELECT * FROM provinces WHERE id = ?`;
+    
     db.query(q, [id], (error, data) => {
         if (error) {
             return res.status(500).send(error);
