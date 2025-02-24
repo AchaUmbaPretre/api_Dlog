9f1a1bc73e3260660e529ff009e15cdd9b06914c
AchaUmbaPretre <achandambi@gmail.com>
2025-02-24 13:32:55 +0100

    commit

--- controllers/permission.controller.js
+++ controllers/permission.controller.js
@@ -662,13 +662,12 @@ exports.getPermissionDeclaration = (req, res) => {
 exports.postPermissionDeclaration = (req, res) => {
   const { id_declaration, id_user,	id_ville, can_view, can_edit, can_comment } = req.body;
 
-
   if (!id_declaration || !id_user) {
     return res.status(400).send({ error: "Les champs 'id_tache' et 'id_user' sont requis." });
   }
 
   try {
-    // Vérifiez si une ligne existe déjà pour id_tache et id_user
+    // Vérifiez si une ligne existe déjà pour id_declaration et id_user
     const qSelect = `SELECT * FROM permissions_declaration WHERE id_declaration = ? AND id_user = ?`;
     const valuesSelect = [id_declaration , id_user];
 
