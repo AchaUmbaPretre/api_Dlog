394e52e4193d507e1086678ce98bc57c59577f22
AchaUmbaPretre <achandambi@gmail.com>
2025-02-24 16:20:23 +0100

    commit

--- controllers/template.controller.js
+++ controllers/template.controller.js
@@ -1171,7 +1171,7 @@ exports.getDeclaration = (req, res) => {
             OR (
                 ds.periode >= DATE_SUB(CURDATE(), INTERVAL 3 MONTH)
                 AND (
-                    (uc.id_user = ${db.escape(userId)} AND uc.can_view = 1)
+                    (pd.id_user = ${db.escape(userId)} AND pd.can_view = 1)
                     OR (pd.id_user = ${db.escape(userId)} AND pd.can_view = 1)
                 )
             )
