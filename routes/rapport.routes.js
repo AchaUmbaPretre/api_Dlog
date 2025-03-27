6af0874f4cd01f943b528ecd6853f864c0330315
AchaUmbaPretre <achandambi@gmail.com>
2025-03-27 17:01:59 +0100

    commit

--- routes/rapport.routes.js
+++ routes/rapport.routes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const rapportController = require('./../controllers/rapport.controller')
-const multer = require('multer');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+
 
 router.get('/', rapportController.getRapport)
 router.get('/one', rapportController.getRapportOne)
