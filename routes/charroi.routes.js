e01f52e47cbca5be866b2179b2d90f19683bbffa
AchaUmbaPretre <achandambi@gmail.com>
2025-04-07 11:23:54 +0100

    commit

--- routes/charroi.routes.js
+++ routes/charroi.routes.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const charroiController = require('./../controllers/charroi.controller');
+
+
+router.get('/cat_vehicule', charroiController.getCatVehicule)
+router.get('/marque', charroiController.getMarque)
+router.get('/modele', charroiController.getModele)
+router.get('/disposition', charroiController.getDisposition)
+router.get('/couleur', charroiController.getCouleur)
 
 module.exports = router;
