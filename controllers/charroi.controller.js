8bd63aeadf7d7643d41b8fe2fc6dfd9c6e4a63fa
AchaUmbaPretre <achandambi@gmail.com>
2025-04-09 11:13:48 +0100

    commit

--- controllers/charroi.controller.js
+++ controllers/charroi.controller.js
@@ -611,6 +611,66 @@ exports.postAffectation = async (req, res) => {
 }
 
 //Controle technique
+exports.getControleTechnique = async (req, res) => {
+    try {
+        const { filtre } = req.query;
+        let whereClause = '';
+
+        switch (filtre) {
+            case 'encours':
+                whereClause = 'WHERE ct.date_validite >= CURDATE()';
+                break;
+            case '3mois':
+                whereClause = `
+                    WHERE ct.date_validite >= CURDATE() 
+                    AND ct.date_validite <= DATE_ADD(CURDATE(), INTERVAL 3 MONTH)
+                `;
+                break;
+            case 'expire':
+                whereClause = 'WHERE ct.date_validite < CURDATE()';
+                break;
+            default:
+                whereClause = '';
+        }
+
+        const query = `
+            SELECT ct.id_controle_tech, ct.date_controle, ct.date_validite, ct.kilometrage, 
+                   ct.ref_controle, ct.resultat, ct.cout_device, ct.cout_ttc, ct.taxe, 
+                   ct.commentaire, v.immatriculation, f.nom AS nom_fournisseur, 
+                   c.nom AS nom_chauffeur, m.nom_marque, tr.type_rep, sct.description,
+                    CASE
+                        WHEN CURDATE() <= ct.date_validite THEN
+                            CASE
+                                WHEN ct.date_validite <= DATE_ADD(CURDATE(), INTERVAL 3 MONTH) THEN 'Expire dans 3 mois'
+                                ELSE 'En cours'
+                            END
+                        ELSE 'Expiré'
+                    END AS statut
+            FROM controle_tech ct
+            INNER JOIN vehicules v ON ct.id_vehicule = v.id_vehicule
+            INNER JOIN marque m ON v.id_marque = m.id_marque
+            INNER JOIN fournisseurs f ON ct.id_fournisseur = f.id_fournisseur
+            INNER JOIN chauffeurs c ON ct.id_chauffeur = c.id_chauffeur
+            INNER JOIN reparation_controle_tech rct ON ct.id_controle_tech = rct.id_controle_technique
+            INNER JOIN type_reparations tr ON sct.id_type_reparation = tr.id_type_reparation
+            ${whereClause} 
+            ORDER BY ct.date_validite ASC
+        `;
+
+        const controle = await queryAsync(query);
+
+        return res.status(200).json({
+            message: 'Liste de controle de technique récupérées avec succès',
+            data: controle,
+        });
+    } catch (error) {
+        console.error('Erreur lors de la récupération des suivie :', error);
+        return res.status(500).json({
+            error: "Une erreur s'est produite lors de la récupération des suivie.",
+        });
+    }
+};
+
 exports.postControlTechnique = async (req, res) => {
     try {
 
