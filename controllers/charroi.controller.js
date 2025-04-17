01a41e214627b2eb362dd08f517da5a3b103bf03
AchaUmbaPretre <achandambi@gmail.com>
2025-04-17 09:05:50 +0100

    commit

--- controllers/charroi.controller.js
+++ controllers/charroi.controller.js
@@ -1185,10 +1185,9 @@ exports.getSubInspection = (req, res) => {
     }
 
     const query = `
-                SELECT sig.id_sub_inspection_gen, sig.montant, tr.type_rep, ci.nom_cat_inspection, cr.nom_carateristique_rep, ig.date_inspection, v.immatriculation, m.nom_marque, sig.id_type_reparation, sig.id_cat_inspection, sig.id_carateristique_rep, sig.img, sig.commentaire, sig.avis, sig.img, tss.nom_type_statut FROM sub_inspection_gen sig
+                SELECT sig.id_sub_inspection_gen, sig.montant, tr.type_rep, ci.nom_cat_inspection, ig.date_inspection, v.immatriculation, m.nom_marque, sig.id_type_reparation, sig.id_cat_inspection, sig.img, sig.commentaire, sig.avis, sig.img, tss.nom_type_statut FROM sub_inspection_gen sig
                     INNER JOIN type_reparations tr ON sig.id_type_reparation = tr.id_type_reparation
                     INNER JOIN cat_inspection ci ON sig.id_cat_inspection = ci.id_cat_inspection
-                    INNER JOIN carateristique_rep cr ON sig.id_carateristique_rep = cr.id_carateristique_rep
                     INNER JOIN inspection_gen ig ON sig.id_inspection_gen = ig.id_inspection_gen
                     INNER JOIN vehicules v ON ig.id_vehicule = v.id_vehicule
                     INNER JOIN marque m ON v.id_marque = m.id_marque
