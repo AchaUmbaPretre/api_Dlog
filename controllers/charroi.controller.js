ef44f3732998b26456f86c6abbc8305407c9231c
AchaUmbaPretre <achandambi@gmail.com>
2025-05-08 14:56:43 +0100

    commit

--- controllers/charroi.controller.js
+++ controllers/charroi.controller.js
@@ -4254,15 +4254,29 @@ exports.getDocumentInspection = (req, res) => {
 //Historique
 exports.getHistorique = (req, res) => {
 
-    const q = `SELECT hv.id_historique, hv.date_action, hv.action, hv.commentaire, u.nom, v.immatriculation, sv.nom_statut_vehicule, m.nom_marque, tr.type_rep, ts.nom_type_statut FROM historique_vehicule hv 
-                LEFT JOIN utilisateur u ON hv.user_cr = u.id_utilisateur
-                LEFT JOIN vehicules v ON hv.id_vehicule = v.id_vehicule
-                LEFT JOIN statut_vehicule sv ON hv.id_statut_vehicule = sv.id_statut_vehicule
-                LEFT JOIN sub_inspection_gen sub ON hv.id_sub_inspection_gen = sub.id_sub_inspection_gen
-                LEFT JOIN type_reparations tr ON sub.id_type_reparation = tr.id_type_reparation
-                LEFT JOIN type_statut_suivi ts ON hv.statut = ts.id_type_statut_suivi
-                LEFT JOIN marque m ON v.id_marque = m.id_marque
-                ORDER BY hv.created_at DESC`;
+    const q = `SELECT 
+            hv.id_historique, 
+            hv.date_action, 
+            hv.action, 
+            hv.commentaire, 
+            u.nom, 
+            v.immatriculation, 
+            sv.nom_statut_vehicule, 
+            m.nom_marque, 
+            COALESCE(tr.type_rep, trs.type_rep) AS type_rep, 
+            ts.nom_type_statut 
+        FROM historique_vehicule hv 
+        LEFT JOIN utilisateur u ON hv.user_cr = u.id_utilisateur
+        LEFT JOIN vehicules v ON hv.id_vehicule = v.id_vehicule
+        LEFT JOIN statut_vehicule sv ON hv.id_statut_vehicule = sv.id_statut_vehicule
+        LEFT JOIN sub_inspection_gen sub ON hv.id_sub_inspection_gen = sub.id_sub_inspection_gen
+        LEFT JOIN sud_reparation sud ON hv.id_sud_reparation = sud.id_sud_reparation
+        LEFT JOIN type_reparations tr ON sub.id_type_reparation = tr.id_type_reparation
+        LEFT JOIN type_reparations trs ON sud.id_type_reparation = trs.id_type_reparation
+        LEFT JOIN type_statut_suivi ts ON hv.statut = ts.id_type_statut_suivi
+        LEFT JOIN marque m ON v.id_marque = m.id_marque
+        ORDER BY hv.created_at DESC;
+      `;
 
     db.query(q, (error, data) => {
         if (error) {
