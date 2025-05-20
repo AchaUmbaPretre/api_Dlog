0855edaafde31f7b8bddd268ce3f9d2aa00421f7
AchaUmbaPretre <achandambi@gmail.com>
2025-05-20 16:14:58 +0100

    commit

--- routes/transporteur.routes.js
+++ routes/transporteur.routes.js
@@ -12,6 +12,7 @@ router.get('/commune', transporteur.getCommune);
 router.get('/ville', transporteur.getVille);
 router.get('/localite', transporteur.getLocalite);
 router.post('/localite', transporteur.postLocalite);
+router.put('/localite', transporteur.putLocalite);
 router.get('/pays', transporteur.getPays);
 
 module.exports = router;
