7af356d64836e30973bbaacc013a81b9adadd368
AchaUmbaPretre <achandambi@gmail.com>
2025-10-03 08:57:10 +0100

    commit

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const eventController = require('./../controllers/event.controller');
 
+router.get('/vehicule_alert', eventController.getAlertVehicule)
+router.put('/vehicule_alert', eventController.putAlertVehicule)
+
 router.get('/', eventController.getEvent);
 router.get('/rapport_day', eventController.getRapportDay);
 router.get('/raw_report', eventController.getRawReport);
