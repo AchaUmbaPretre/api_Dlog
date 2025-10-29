8d12794d79bdc097fd723cfffb1da70aa884e1c7
AchaUmbaPretre <achandambi@gmail.com>
2025-10-29 13:41:47 +0100

    commit

--- controllers/geofences.controller.js
+++ controllers/geofences.controller.js
@@ -9,3 +9,28 @@ exports.getGeofences = (req, res) => {
         return res.status(200).json(data);
     });
 }
+
+exports.postGeofences = async (req, res) => {
+    const { falcon_id, nom_falcon, nom, type_geofence, client_id, zone_parent_id, description, actif } = req.body;
+
+    try {
+        const q = 'INSERT INTO geofences_dlog(`falcon_id`, `nom_falcon`, `nom`, `type_geofence`, `client_id`, `zone_parent_id`, `description`, `actif`) VALUES(?,?,?,?,?,?,?,?)';
+
+        const values = [
+            falcon_id, 
+            nom_falcon, 
+            nom, 
+            type_geofence, 
+            client_id, 
+            zone_parent_id, 
+            description, 
+            actif
+        ];
+
+        await db.query(q, values);
+        return res.status(201).json({ message: 'Projet ajouté avec succès'});
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout du nouveau projet:', error);
+        return res.status(500).json({ error: "Une erreur s'est produite lors de l'ajout de la tâche." });
+    }
+};
