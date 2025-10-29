5e733da69a831240b20099150b7691d11f42e6f0
AchaUmbaPretre <achandambi@gmail.com>
2025-10-29 13:28:27 +0100

    commit

--- controllers/geofences.controller.js
+++ controllers/geofences.controller.js
@@ -1,1 +1,11 @@
 const { db } = require("./../config/database");
+
+exports.getGeofences = (req, res) => {
+    
+    let q = `SELECT * FROM geofences_dlog`;
+     
+    db.query(q, (error, data) => {
+        if (error) res.status(500).send(error);
+        return res.status(200).json(data);
+    });
+}
--- routes/geofences.routes.js
+++ routes/geofences.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const geofencesController = require('./../controllers/geofences.controller')
 
-
+router.get('/', geofencesController.getGeofences)
 
 module.exports = router;
