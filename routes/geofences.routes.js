2210cf3f7c08b2a41cec9fbc7f7e62c1fb807eb9
AchaUmbaPretre <achandambi@gmail.com>
2025-10-29 14:31:37 +0100

    commit

--- routes/geofences.routes.js
+++ routes/geofences.routes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const geofencesController = require('./../controllers/geofences.controller')
 
 router.get('/', geofencesController.getGeofences)
+router.post('/', geofencesController.postGeofences)
+router.put('/', geofencesController.updateGeofences)
+
 
 module.exports = router;
