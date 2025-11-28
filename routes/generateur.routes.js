1eaad6efe5c7b07208672980769a3d4501fd7578
AchaUmbaPretre <achandambi@gmail.com>
2025-11-28 15:26:56 +0100

    commit

--- routes/generateur.routes.js
+++ routes/generateur.routes.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const generateurController = require('../controllers/generateur.controller')
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const filename = uuidv4() + ext;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage });
 
 router.get('/', generateurController.getGenerateur)
 router.get('/one', generateurController.getGenerateurOne)
-router.post('/', generateurController.postGenerateur)
+router.post('/', upload.array('img', 10), generateurController.postGenerateur)
  
 module.exports = router;
