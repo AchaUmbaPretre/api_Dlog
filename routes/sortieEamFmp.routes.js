bc42adeea54936f1f70e5bdd65a20e8876c69837
AchaUmbaPretre <achandambi@gmail.com>
2026-01-06 14:27:18 +0100

    commit

--- routes/sortieEamFmp.routes.js
+++ routes/sortieEamFmp.routes.js
@@ -2,6 +2,23 @@ const express = require("express");
 const router = express.Router();
 const sortieEamFmpController = require('./../controllers/sortieEamFmp.controller');
 
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const filename = uuidv4() + ext;
+        cb(null, filename);
+    }
+});
+const upload = multer({ storage });
+
 router.get('/eam', sortieEamFmpController.getSortieEam);
 router.get('/by_smr_eam', sortieEamFmpController.getSortieEamBySmr);
 router.put('/put_eam', sortieEamFmpController.putSortieEam);
@@ -18,5 +35,9 @@ router.get('/global_item', sortieEamFmpController.getGlobalItemsReconciliation);
 
 router.post('/eam_post', sortieEamFmpController.postEamDocPhysique);
 router.post('/fmp_post', sortieEamFmpController.postFmpDocPhysique);
+
+//EXPORT EXCEL
+router.post('/fmp_excel', upload.array('chemin_document', 10), sortieEamFmpController.postSortiefmpExcel);
+router.post('/eam_excel', upload.array('chemin_document', 10), sortieEamFmpController.postEamExcel);
 
 module.exports = router;
