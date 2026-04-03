59c56474d09bea529c945b2c506e9c13853c4122
AchaUmbaPretre <achandambi@gmail.com>
2026-04-03 14:50:31 +0100

    commit

--- routes/notification.routes.js
+++ routes/notification.routes.js
@@ -1,14 +1,11 @@
-// routes/notificationRoutes.js
 const express = require("express");
 const router = express.Router();
 const notification = require('./../controllers/notification.controller');
 const { authorize } = require("../midllewares/authorize");
 
-// Routes existantes
 router.post('/register',authorize('attendance.events.read'), notification.postNotifications);
 router.delete('/unregister', notification.DesactiveToken);
 
-// Nouvelles routes
 router.post('/send-to-user', notification.sendToUser);
 router.post('/send-to-many', notification.sendToMany);
 router.post('/send-to-all', notification.sendToAll);
